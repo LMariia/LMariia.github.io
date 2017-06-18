66050559fa55fc7f3095a55f1a4a3a5b70200a6d
Mariia <mariia.lobachova@gmail.com>
2017-06-18 15:48:21 +0300

    Fixed eslint errors

--- scripts.js
+++ scripts.js
@@ -1,20 +1,15 @@
 var table = document.getElementsByTagName('tbody');
-var loadUsers = getUsers(function (users) {
-    renderUsers(users);
-});
 
-loadUsers();
+var getUsers = function (callback) {
+    var since = 0;
 
-function getUsers(callback) {
-    var since = 0 ;
-
-    return function() {
+    return function () {
         var request = new XMLHttpRequest();
 
         request.onreadystatechange = function () {
             if (request.readyState === 4) {
                 if (request.status === 200) {
-                    if (typeof callback == "function") {
+                    if (typeof callback === 'function') {
                         callback(JSON.parse(request.responseText));
                     }
                 } else {
@@ -27,85 +22,36 @@ function getUsers(callback) {
         request.send();
 
         since += 31;
-    }
+    };
 }
 
-function renderUsers(users) {
-    users.forEach(function(user){
-        var userBlock = document.createElement('tr');
-        var detailsBlock = document.createElement('tr');
-        detailsBlock.className += 'hide';
-
-        userBlock.appendChild( createRow('img', {src: user.avatar_url}) );
-        userBlock.appendChild( createRow('a', {href: user.html_url, onclick: function () { event.stopPropagation(); }}, user.login) );
-        userBlock.appendChild( createRow('span', {}, user.site_admin) );
-
-        userBlock.addEventListener('click', function(){
-            showDetails(this, user.url);
-        });
-
-        table[0].appendChild(userBlock);
-        table[0].appendChild(detailsBlock);
-    });
-}
-
-function createRow(tagName, attrs, text) {
-    var td = document.createElement('td');
-
-    td.appendChild( createEl(tagName, attrs, text) );
-
-    return td;
-};
-
-function createEl(tagName, attrs, text) {
+var createEl = function (tagName, attrs, text) {
     var el = document.createElement(tagName);
 
     Object.assign(el, attrs);
 
-    if (arguments.length == 3) {
+    if (arguments.length === 3) {
         el.innerText = text;
     }
 
     return el;
 }
 
-function showDetails(userBlock, userUrl){
-    var detailsBlock = userBlock.nextSibling;
-    var user;
+var createRow = function (tagName, attrs, text) {
+    var td = document.createElement('td');
 
-    userBlock.classList.toggle('user-selected');
-    detailsBlock.classList.toggle('hide');
+    td.appendChild(createEl(tagName, attrs, text));
 
-    if ( detailsBlock.innerHTML === "" ) {
-        getUser(userUrl, function (user) {
-            var links = document.createElement('td');
-            var fragment = document.createDocumentFragment();
-
-            links.className += ' links';
-
-            fragment.appendChild( createEl('a', {href: user.followers_url}, 'Followers') );
-            fragment.appendChild( createEl('a', {href: user.following_url}, 'Followings') );
-            fragment.appendChild( createEl('a', {href: user.starred_url}, 'Starred') );
-            fragment.appendChild( createEl('a', {href: user.subscriptions_url}, 'Subscriptions') );
-            fragment.appendChild( createEl('a', {href: user.organizations_url}, 'Organizations') );
-            fragment.appendChild( createEl('a', {href: user.repos_url}, 'Repos') );
-
-            links.appendChild( fragment );
-
-            detailsBlock.appendChild( createRow('span', {}, user.name || '(no name)'));
-            detailsBlock.appendChild( createRow('span', {}, user.email || '(no email)'));
-            detailsBlock.appendChild( links );
-        });
-    }
+    return td;
 }
 
-function getUser(userUrl, callback) {
+var getUser = function (userUrl, callback) {
     var request = new XMLHttpRequest();
 
     request.onreadystatechange = function () {
         if (request.readyState === 4) {
             if (request.status === 200) {
-                if (typeof callback == "function") {
+                if (typeof callback === 'function') {
                     callback(JSON.parse(request.responseText));
                 }
             } else {
@@ -117,4 +63,58 @@ function getUser(userUrl, callback) {
     request.open('Get', userUrl);
     request.send();
 }
+
+var showDetails = function (userBlock, userUrl) {
+    var detailsBlock = userBlock.nextSibling;
+
+    userBlock.classList.toggle('user-selected');
+    detailsBlock.classList.toggle('hide');
+
+    if (detailsBlock.innerHTML === '') {
+        getUser(userUrl, function (user) {
+            var links = document.createElement('td');
+            var fragment = document.createDocumentFragment();
+
+            links.className += ' links';
+
+            fragment.appendChild(createEl('a', { href: user.followers_url }, 'Followers'));
+            fragment.appendChild(createEl('a', { href: user.following_url }, 'Followings'));
+            fragment.appendChild(createEl('a', { href: user.starred_url }, 'Starred'));
+            fragment.appendChild(createEl('a', { href: user.subscriptions_url }, 'Subscriptions'));
+            fragment.appendChild(createEl('a', { href: user.organizations_url }, 'Organizations'));
+            fragment.appendChild(createEl('a', { href: user.repos_url }, 'Repos'));
+
+            links.appendChild(fragment);
+
+            detailsBlock.appendChild(createRow('span', {}, user.name || '(no name)'));
+            detailsBlock.appendChild(createRow('span', {}, user.email || '(no email)'));
+            detailsBlock.appendChild(links);
+        });
+    }
+}
+
+var renderUsers = function (users) {
+    users.forEach(function (user) {
+        var userBlock = document.createElement('tr');
+        var detailsBlock = document.createElement('tr');
+        detailsBlock.className += 'hide';
+
+        userBlock.appendChild(createRow('img', { src: user.avatar_url }));
+        userBlock.appendChild(createRow('a', { href: user.html_url, onclick: function () { event.stopPropagation(); } }, user.login));
+        userBlock.appendChild(createRow('span', {}, user.site_admin));
+
+        userBlock.addEventListener('click', function(){
+            showDetails(this, user.url);
+        });
+
+        table[0].appendChild(userBlock);
+        table[0].appendChild(detailsBlock);
+    });
+}
+
+var loadUsers = getUsers(function (users) {
+    renderUsers(users);
+});
+
+loadUsers();
 
